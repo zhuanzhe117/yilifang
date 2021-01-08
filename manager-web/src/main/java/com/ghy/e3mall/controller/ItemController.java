258fec5cf2f6fabefbf24debce0cfb1299b41570
haiyangu <haiyangu@huashenghaoche.com>
2021-01-08 13:01:25 +0800

    controller层实体能直接转为json数据传到页面

--- manager-web/src/main/java/com/ghy/e3mall/controller/ItemController.java
+++ manager-web/src/main/java/com/ghy/e3mall/controller/ItemController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
